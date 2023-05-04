4e15ea2d73cdb1d6a320730c9b98d9e78bc21d73
Antonio Mauricio <oantoniomauricio@gmail.com>
2023-05-04 11:23:05 +0100

    organize src folder and add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("test 1");
+import './normalize.css';
+
+function component() {
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
+
+  btn.innerHTML = "Click me and check the console!";
+  element.classList.add('hello');
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
